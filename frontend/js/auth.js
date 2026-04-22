24f3556168dc466c8f29bff06ce4b8721fdea893
C3PHEI <m.max32@yahoo.com>
2026-04-22 09:59:18 +0200

    Entra id configuration in auth.js

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -6,27 +6,25 @@ const msalConfig = {
   auth: {
     clientId: "d0795c1d-3049-4ed5-b613-48df870a10c6",
     authority: "https://login.microsoftonline.com/79686dd2-7fae-4fd8-83a2-40198e28d933",
-    redirectUri: "http://localhost:5500/index.html"
+    redirectUri: "http://localhost:5000/index.html"
   },
   cache: { cacheLocation: "sessionStorage" }
 };
 
-const apiScopes = ["api://d0795c1d-3049-4ed5-b613-48df870a10c6"];
+const apiScopes = ["api://d0795c1d-3049-4ed5-b613-48df870a10c6/access_as_user"];
 
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
 async function ensureLogin() {
   await msalInstance.initialize();
 
-  // Redirect-Rückkehr abfangen
   const result = await msalInstance.handleRedirectPromise();
   if (result) msalInstance.setActiveAccount(result.account);
 
   const accounts = msalInstance.getAllAccounts();
   if (accounts.length === 0) {
-    // Kein Account → sofort zu Microsoft
     await msalInstance.loginRedirect({ scopes: apiScopes });
-    return; // Seite wird weggeleitet
+    return;
   }
   msalInstance.setActiveAccount(accounts[0]);
 }
@@ -41,5 +39,4 @@ async function getApiToken() {
   }
 }
 
-// Login-Gate gleich starten
 ensureLogin();
